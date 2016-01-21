f6a0412226402d37f36adb2327d4160102044bb6
Mike Doran <mikeybyker@gmail.com>
2016-01-21 09:56:32 +0000

    -

--- app/js/constants.ts
+++ app/js/constants.ts
@@ -1,5 +1,5 @@
 export const LASTFM = {
     apiEndpoint: 'http://ws.audioscrobbler.com/2.0/',
-    api_key: '636d81e5364ebc98a99d202c57268f18',
+    api_key: 'YOUR_API_KEY',
     format: 'json'
 };
