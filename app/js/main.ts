45e8ef22f29e8f158701c266154306dd4738ee08
Mike Doran <mikeybyker@gmail.com>
2016-08-06 14:05:43 +0100

    Fix typo

--- app/js/main.ts
+++ app/js/main.ts
@@ -1,9 +1,10 @@
 import {bootstrap}                 from '@angular/platform-browser-dynamic';
 import {HTTP_PROVIDERS}            from '@angular/http';
-import {AppComponent}              from './app.component';
-import {appRouterProviders}        from './app.routes';
 import {disableDeprecatedForms,
         provideForms }             from '@angular/forms';
+
+import {AppComponent}              from './app.component';
+import {appRouterProviders}        from './app.routes';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/forkJoin';
