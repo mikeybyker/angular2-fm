e9797e72ad9a3caf5a07f28879705b796ea25ad8
Mike Doran <mikeybyker@gmail.com>
2016-09-14 16:17:12 +0100

    Add a check for api_key

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import { platformBrowserDynamic }    from '@angular/platform-browser-dynamic';
 import { enableProdMode }            from '@angular/core';
 import { environment }               from './environments/environment';
 import { AppModule }                 from './app/';
+import { LastFMConfig }              from './app/lastfm.config';
 
 if (environment.production) {
     enableProdMode();
 }
-
-platformBrowserDynamic().bootstrapModule(AppModule);
+if(LastFMConfig[0].useValue.api_key === 'YOUR_API_KEY'){
+    alert('You need to add an api_key to lastfm.config.ts');
+} else {
+    platformBrowserDynamic().bootstrapModule(AppModule);
+}
